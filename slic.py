6493322a335cc34fbaefdf9227e2b39f1c4cb3bf
Aneeq-Younas <85731812+Aneeq-Younas@users.noreply.github.com>
2021-09-13 23:10:29 -0700

    Add files via upload

---
+++ slic.py
@@ -0,0 +1,19 @@
+myStr= "I am a good boy"
+print(myStr)                        #simple print for myStr
+print(myStr[3])                     #Give 3rd word of above str
+print(myStr[0:6])                   #first six character 0 to 6
+print(len(myStr))                   #length of whole str
+print(myStr.endswith("boy"))        #return true false based on statnment
+print(myStr.endswith("vboy"))       #return false cez not endwith vboy
+print(myStr.count("o"))             #will tell how many times o in str
+print(myStr.capitalize())           #first letter does captial of str if it does small
+print(myStr.find("am"))             #to find any character in huge str
+print(myStr.lower())                #give whole str in lower letters
+print(myStr.upper())                #give whole str in upper letters
+print(myStr.replace("am", "is"))    #replace any letter to another one
+
+
+StringFunctions
+
+Some slicing the some functions
+
